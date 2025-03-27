6f3d17a7961bf33fea65ad5c586e44a901549390
DyingHynixMLC <139165556+DyingHynixMLC@users.noreply.github.com>
2025-03-27 20:16:03 +0100

    Update tarbstall.sh

--- tarbstall.sh
+++ tarbstall.sh
@@ -1,4 +1,4 @@
-help="We do not have any commands yet :skull:"
+help="We do not have any commands yet :skull:, to get commands you should jump off a cliff. that will immediately grant you commands."
 
 if [ ! -f inited ]; then
     bash init.sh
