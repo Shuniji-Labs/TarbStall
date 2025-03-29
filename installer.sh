5a2113e3f59accd9a95a69ff95a22da4af13f11c
DyingHynixMLC <139165556+DyingHynixMLC@users.noreply.github.com>
2025-03-29 11:27:12 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,20 +4,12 @@ RED='\033[0;31m'
 YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m'
-
-echo_red() {
-    echo -e "${RED}$1${NC}"
-}
-
-echo_yellow() {
-    echo -e "${YELLOW}$1${NC}"
-}
-
-echo_green() {
-    echo -e "${GREEN}$1${NC}"
-}
-
-echo -e "\nTarbStall Installer\n"
+echo_red() { echo -e "${RED}$1${NC}" }
+echo_yellow() { echo -e "${YELLOW}$1${NC}" }
+echo_green() { echo -e "${GREEN}$1${NC}" }
+clear
+tput cup 0 0
+echo -e "TarbStall Installer\n"
 sleep 2
 
 if [[ $EUID -ne 0 ]]; then
@@ -27,22 +19,22 @@ fi
 
 install_tarbstall() {
     echo -e "\nSetting up directories..."
-    mkdir /usr/local/bin/TarbStall || { echo_red "Failed to create directory."; exit 0; }
+    mkdir /usr/local/bin/TarbStall || { echo_red "oh noezies i failed making the directory shit oh god no bad"; exit 0; }
 
     echo -e "\nDownloading TarbStall..."
-    git clone -q https://github.com/Shuniji-Labs/TarbStall.git /usr/local/bin/TarbStall || { echo_red "Download failed."; exit 0; }
+    git clone -q https://github.com/Shuniji-Labs/TarbStall.git /usr/local/bin/TarbStall || { echo_red "damn vro the download failed, you sure u have internet?"; exit 0; }
 
     echo -e "\nCreating symlink..."
-    ln -s /usr/local/bin/TarbStall/tarbstall.sh /usr/local/bin/tarbstall || echo_red "Failed to create symlink."
+    ln -s /usr/local/bin/TarbStall/tarbstall.sh /usr/local/bin/tarbstall || echo_yellow "jesus christ bro this symlink aint working shit."
 
     echo -e "\nPreparing temporary storage..."
-    mkdir /usr/local/bin/TarbStall/tmp || echo_yellow "Could not create temporary directory. Run: sudo mkdir /usr/local/bin/TarbStall/tmp"
+    mkdir /usr/local/bin/TarbStall/tmp || echo_yellow "god damn the tmp directory didn't wanna get made, anyway run this command: sudo mkdir /usr/local/bin/TarbStall/tmp"
 
     echo -e "\nSetting permissions..."
-    chmod +x /usr/local/bin/tarbstall || echo_red "Failed to set executable permissions."
+    chmod +x /usr/local/bin/tarbstall || echo_red "chmod +x jumped off a cliff somehow."
 }
 
-echo -e "Install TarbStall? (Y/n)"
+echo -e "would you like to install TarbStall? (y/n)"
 read -p "> " user_choice
 
 if [[ $user_choice == "n" || $user_choice == "N" ]]; then
@@ -53,4 +45,4 @@ else
     exit 0
 fi
 
-echo_green "\nTarbStall installation complete!"
+echo_green "TarbStall installation complete!"
