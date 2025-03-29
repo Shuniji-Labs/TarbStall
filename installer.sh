9151144d8ddf6d0d979c0b68be89782f8ed53055
Simon <133875585+simpansoftware@users.noreply.github.com>
2025-03-29 10:58:00 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,10 +5,10 @@ read install
 if [ $install = n ]; then
     exit 0
 else
-    mkdir /usr/local/bin/TarbStall
-    mkdir /usr/local/bin/TarbStall/tmp
+    sudo mkdir /usr/local/bin/TarbStall
     sudo git clone https://github.com/Shuniji-Labs/TarbStall.git /usr/local/bin/TarbStall
     sudo ln -s /usr/local/bin/TarbStall/tarbstall.sh /usr/local/bin/tarbstall
+    sudo mkdir /usr/local/bin/TarbStall/tmp
     sudo chmod +x /usr/local/bin/tarbstall
 fi
 echo TarbStall should be installed!
