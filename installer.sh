1542036891f01e58b0c32780a498dd7a5c8bdc05
DyingHynixMLC <139165556+DyingHynixMLC@users.noreply.github.com>
2025-03-29 11:09:07 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,14 +1,20 @@
-echo TarbStall Installer
+echo -e "TarbStall Installer\n"
 sleep 3
-echo "Welcome to the TarbStall installer. Would you like to install TarbStall? Y/n"
-read install
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    echo "Breaking.."
+    exit 0
+fi
+
+echo "Welcome to the TarbStall installer. Would you like to install TarbStall? (Y/n)"
+read -p "> " install
 if [ $install = n ]; then
     exit 0
 else
-    sudo mkdir /usr/local/bin/TarbStall
-    sudo git clone https://github.com/Shuniji-Labs/TarbStall.git /usr/local/bin/TarbStall
-    sudo ln -s /usr/local/bin/TarbStall/tarbstall.sh /usr/local/bin/tarbstall
-    sudo mkdir /usr/local/bin/TarbStall/tmp
-    sudo chmod +x /usr/local/bin/tarbstall
+    mkdir /usr/local/bin/TarbStall
+    git clone https://github.com/Shuniji-Labs/TarbStall.git /usr/local/bin/TarbStall
+    ln -s /usr/local/bin/TarbStall/tarbstall.sh /usr/local/bin/tarbstall
+    mkdir /usr/local/bin/TarbStall/tmp
+    chmod +x /usr/local/bin/tarbstall
 fi
-echo TarbStall should be installed!
+echo "TarbStall should be installed!"
