e6af67ef7a02d5799ecede1caaf998984661ce03
Simon <133875585+simpansoftware@users.noreply.github.com>
2025-03-29 13:32:07 +0100

    Update steam.sh

--- packages/steam.sh
+++ packages/steam.sh
@@ -2,6 +2,8 @@ cd /usr/local/bin/TarbStall/tmp
 curl -O https://repo.steampowered.com/steam/archive/precise/steam_latest.tar.gz
 tar -xvzf steam_latest.tar.gz
 cd steam-launcher
+./configure
+make
 sudo make install
 cd ..
 rm -rf steam-launcher
