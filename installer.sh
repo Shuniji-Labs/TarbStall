fe9f13ebb5c18023b32aded6ca306a16a7498489
DyingHynixMLC <139165556+DyingHynixMLC@users.noreply.github.com>
2025-03-29 11:18:13 +0100

    overhauling installer

--- installer.sh
+++ installer.sh
@@ -1,20 +1,48 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo_red() {
+    echo -e "${RED}$1${NC}"
+}
+
+echo_yellow() {
+    echo -e "${YELLOW}$1${NC}"
+}
+
+echo_green() {
+    echo -e "${GREEN}$1${NC}"
+}
+
 echo -e "TarbStall Installer\n"
 sleep 3
+
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root."
+    echo_red "This script must be run as root."
     echo "Breaking.."
     exit 0
 fi
 
+install_tarbstall() {
+    mkdir /usr/local/bin/TarbStall || { echo_red "An error has occurred: MKDir failed."; exit 0; }
+    git clone https://github.com/Shuniji-Labs/TarbStall.git /usr/local/bin/TarbStall || { echo_red "Failed cloning TarbStall!"; exit 0; }
+    ln -s /usr/local/bin/TarbStall/tarbstall.sh /usr/local/bin/tarbstall || echo_red "Failed making symlink!"
+    mkdir /usr/local/bin/TarbStall/tmp || echo_yellow "Failed to make temporary path. Please, open a new terminal and run 'sudo mkdir /usr/local/bin/TarbStall/tmp'"
+    chmod +x /usr/local/bin/tarbstall || echo_red "Failed to give executable permission."
+}
+
 echo "Welcome to the TarbStall installer. Would you like to install TarbStall? (Y/n)"
-read -p "> " install
-if [ $install = n ]; then
+read -p "> " user_choice
+
+if [[ $user_choice == "n" || $user_choice == "N" ]]; then
     exit 0
+elif [[ $user_choice == "y" || $user_choice == "Y" ]]; then
+    install_tarbstall
 else
-    mkdir /usr/local/bin/TarbStall
-    git clone https://github.com/Shuniji-Labs/TarbStall.git /usr/local/bin/TarbStall
-    ln -s /usr/local/bin/TarbStall/tarbstall.sh /usr/local/bin/tarbstall
-    mkdir /usr/local/bin/TarbStall/tmp
-    chmod +x /usr/local/bin/tarbstall
+    exit 0
 fi
-echo "TarbStall should be installed!"
+
+echo_green "TarbStall should be installed!"
