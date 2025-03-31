35d2e585933d9acf0f71d5abd6cd5acf9a0efe20
Simon <133875585+simpansoftware@users.noreply.github.com>
2025-03-31 16:36:28 +0200

    Update steam.sh

--- packages/linux/steam.sh
+++ packages/linux/steam.sh
@@ -11,3 +11,4 @@ sudo make install DESTDIR=/usr/local/bin/TarbStall/packages/installed/steam
 cd ..
 rm -rf steam-launcher
 rm -f steam-latest.tar.gz
+ln -s /usr/local/bin/TarbStall/packages/installed/steam/usr/bin /usr/local/bin/steam2
