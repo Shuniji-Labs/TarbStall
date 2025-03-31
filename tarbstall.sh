016e6a0ace17c96171365dffc531227ff1896469
Simon <133875585+simpansoftware@users.noreply.github.com>
2025-03-31 13:45:05 +0200

    Update tarbstall.sh

--- tarbstall.sh
+++ tarbstall.sh
@@ -46,7 +46,9 @@ elif [ "$1" = update ]; then
     mkdir "/tmp/tarbstall-update"
     git clone https://github.com/Shuniji-Labs/TarbStall.git /tmp/tarbstall-update
     sudo cp -vr /tmp/tarbstall-update/* "/usr/local/bin/TarbStall"
-    rm -rf "/tmp/tarbstall-update"    
+    rm -rf "/tmp/tarbstall-update" 
+elif [ "$1" = directory ]; then
+    cd /usr/local/bin/TarbStall   
 elif [ -z "$1" ]; then
     echo "$help"
 fi
